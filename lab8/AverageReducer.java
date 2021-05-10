0a5e0150cbd92c7ed0802694df716bf10c910351
POOJA SRINIVASAN <53825520+PoojaSrinivasan18@users.noreply.github.com>
2021-05-10 15:33:04 +0530

    Add files via upload

---
+++ lab8/AverageReducer.java
@@ -0,0 +1,24 @@
+package temperature;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.*;
+import java.io.IOException;
+
+public class AverageReducer extends Reducer <Text, IntWritable,Text,
+IntWritable >
+ {
+public void reduce(Text key,  Iterable<IntWritable> values, Context
+context) throws IOException,
+            InterruptedException
+            {
+            int max_temp = 0;
+            int count = 0;
+            for (IntWritable value : values)
+                        {
+                                    max_temp += value.get();
+                                    count+=1;
+                        }
+            context.write(key, new IntWritable(max_temp/count));
+            }
+}
+
